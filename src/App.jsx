d6deb5eea071d974b0b2a3083d33aefb64f9a415
aisuluu <aisuluubekeeva.0555@gmail.com>
2024-09-10 17:02:28 +0300

    create hw1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import "./components/Main";
+import { Main } from "./components/Main";
+// import { Mnpain } from "./components/Main";
+function App() {
+  return (
+    <div>
+      {/* <Mnpain /> */}
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,88 @@
+import "./Main.css";
+
+export function Main() {
+  return (
+    <div id="header">
+      <div id="item">
+        <h4> Трикатажное платьке в полоску</h4>
+      </div>
+      <section className="container">
+     
+          <img
+            id="img1"
+            src="https://i.pinimg.com/736x/e6/a6/6e/e6a66e331d430fdab72b5fa6e0850adf.jpg"
+            alt=""
+          />
+      
+        <article>
+        <div id="folse">
+          <p id="tekst1">цвет в наличии</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div id="texts1">
+          <p>Размеры в наличии</p>
+          <p>XXS,XS,S,M,L,XL,XXL</p>
+        </div>
+        </article>
+       
+      </section>
+      <h4> Трикатажное платье</h4>
+      <section  className="container">
+
+          <img
+            id="img1"
+            src="https://i.pinimg.com/736x/3a/09/57/3a0957c713d8324011785f79befbc0fc.jpg"
+            alt=""
+          />
+    
+        <article>
+        <div className="display">
+          <p id="tekst1">цвет в наличии</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div id="texts1">
+          <p>Размеры в наличии</p>
+          <p>XXS,XS,S,M,L,XL,XXL</p>
+        </div>
+        </article>
+        
+      </section>
+      <h4> платье в полоску</h4>
+      <section  className="container">
+       
+          <img
+            id="img1"
+            src="https://shop.mango.com/assets/rcs/pics/static/T5/fotos/S/57075791_48.jpg?imwidth=2048&imdensity=1&ts=1690221787919"
+            alt=""
+          />
+      
+        <article>
+        <div className="display">
+          <p id="tekst1">цвет в наличии</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div id="texts1">
+          <p>Размеры в наличии</p>
+          <p>XXS,XS,S,M,L,XL,XXL</p>
+        </div>
+        </article>
+
+      </section>
+    </div>
+  );
+}
